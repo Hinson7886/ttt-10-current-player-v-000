b3dedbcaacb2cdcc805b73b81f36429143d0d8b3
Jon Hinson <jonhinsonfx@gmail.com>
2017-10-13 19:16:12 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
-   counter = 0
-   until counter == 3
+   number_of_turns = 0
+   until number_of_turns == 3
    board.each do |board|
-   counter += 1
+   number_of_turns += 1
   end
 end

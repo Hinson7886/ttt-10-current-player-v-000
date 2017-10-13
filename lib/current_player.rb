0c282ce407cf93ac7e238c3dfb5cc7c4b4dc3eb7
Jon Hinson <jonhinsonfx@gmail.com>
2017-10-13 19:13:12 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,12 +2,9 @@ def turn_count(board)
   counter = 0
   until counter == 3
   board.each do |board|
-  counter += 1
+  counter += 1  
   end
 end
 
 
-primary_colors = ["Red", "Yellow", "Blue"]
-primary_colors.each do |color|
-  puts "Primary Color #{color} is #{color.length} letters long."
-end
+

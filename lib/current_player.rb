a169fc59f7097cf0f7481f2dfe2bdb5ab5330dac
Jon Hinson <jonhinsonfx@gmail.com>
2017-10-13 19:04:58 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,2 +1,11 @@
 def turn_count(board)
+  board.each do | board |
+  
+  end
+end
+
+
+primary_colors = ["Red", "Yellow", "Blue"]
+primary_colors.each do |color|
+  puts "Primary Color #{color} is #{color.length} letters long."
 end
